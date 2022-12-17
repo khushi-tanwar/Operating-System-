f4f928dc0b42bbcc66d2206918e461d2daa3f71a
khushi-tanwar <104456509+khushi-tanwar@users.noreply.github.com>
2022-12-17 23:07:26 +0530

    Practical Q1-(a).c

---
+++ Practical Q1-(a).c
@@ -0,0 +1,23 @@
+Write a program (using fork() and/or exec() commands) where parent and child execute:
+ a) same program, same code.
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+int main()
+{
+int a;
+a=fork();
+if(a<0)
+{
+printf("Child Process could not be Created");
+exit(-1);
+}
+else //Child Process Created Successfully
+{
+printf("My ID is:%d,My Parent ID is:%d\n",getpid(),getppid());
+}
+return 0;
+}
