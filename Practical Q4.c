17ed9349c3709d717adc85fb43c612962b3ef498
khushi-tanwar <104456509+khushi-tanwar@users.noreply.github.com>
2022-12-18 16:42:45 +0530

    Practical Q4.c

---
+++ Practical Q4.c
@@ -0,0 +1,25 @@
+Write a program to print file details including owner access permissions, file access time, 
+where file name is given as argument.
+
+
+#include<stdio.h>
+#include<sys/stat.h>
+#include<time.h>
+int main(int argc,char *argv[3])
+{
+int i;
+struct stat buffer;
+printf("GIve File Name:");
+for(i=1;i<argc;i++)
+{
+printf("file=%s\n",argv[i]);
+if(stat(argv[i],&buffer)<0)
+printf("Error in File Started");
+else
+{
+printf("Ownered=%d\ngid=%d\n",buffer.st_uid,buffer.st_gid);
+printf("Access Permission=%d\n",buffer.st_mode);
+printf("Access Time=%d\n",(time(&(buffer.st_atime))));
+}
+}
+}
